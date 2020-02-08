41d1708f001c8d02fefe9199daeb1f525f05b0c7
rifkyar <rifkyrachman21@gmail.com>
2020-02-08 11:02:56 +0700

    add calendar view

---
+++ EventReminder/Controllers/FullCalendarController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EventReminder.Controllers
+{
+    public class FullCalendarController : Controller
+    {
+        // GET: FullCalendar
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
